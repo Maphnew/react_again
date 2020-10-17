1d21a83ceacccbfe4e20d1b4ca5086bb59703306
maphnew <zcm3118@gmail.com>
2020-10-17 17:44:02 +0900

    12. Conditional Rendering in JSX

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,14 +1,92 @@
-"use strict";
+'use strict';
 
 console.log("This app is running~!~!~!");
 
 // JSX
-
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+};
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                app.options[0]
+            ),
+            React.createElement(
+                'li',
+                null,
+                app.options[1]
+            )
+        ) : 'No options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
 );
+
+var user = {
+    name: 'Mike',
+    age: '100',
+    location: 'Ulsan'
+};
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,8 +1,45 @@
 console.log("This app is running~!~!~!");
 
 // JSX
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+}
+var template = (
+<div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? (<ol>
+            <li>{app.options[0]}</li>
+            <li>{app.options[1]}</li>
+        </ol>) : 'No options'}
+    </p>
+    <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+    </ol>
+</div>
+);
 
-var template = <h1>Indecision App</h1>;
+var user = {
+    name: 'Mike',
+    age: '100',
+    location: 'Ulsan'
+}
+function getLocation(location) {
+    if(location) {
+        return <p>Location: {location}</p>;
+    }
+}
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous' }</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+)
+
 var appRoot = document.getElementById('app')
 
 ReactDOM.render(template, appRoot)
