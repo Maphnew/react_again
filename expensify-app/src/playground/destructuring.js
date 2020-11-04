a5194bb364961f20d52321a0b4719fd45756493d
maphnew <zcm3118@gmail.com>
2020-11-04 22:21:52 +0900

    89. ES6 Array Destructuring

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,3 +1,5 @@
+// OBJECT
+
 const book = {
     title: 'Ego is the Enemy',
     author: 'Ryan Holiday',
@@ -9,3 +11,12 @@ const book = {
 const { name: publisherName = 'Self-Published' } = book.publisher
 
 console.log(publisherName)
+
+
+// ARRAY
+
+
+const item = ['Coffee (hot)', '$2.00', '$2.40', '$3.00']
+const [menu, small, medium, large] = item
+
+console.log(`A medium ${menu} costs ${medium}`)
