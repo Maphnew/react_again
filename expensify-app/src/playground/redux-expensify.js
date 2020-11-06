74001cd14fb2712aef0f4c9f77236e968e6c4276
maphnew <zcm3118@gmail.com>
2020-11-06 21:43:19 +0900

    96. Filtering Redux Data

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -130,6 +130,21 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
     }
 }
 
+// timestamps (milliseconds)
+// Jan 1st 1970 (unix epoch)
+
+
+// Get visible expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate}) => {
+    return expenses.filter((expense) => {
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+        return startDateMatch && endDateMatch && textMatch;
+    })
+}
+
 // Store creation
 
 const store = createStore(
@@ -140,33 +155,37 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-    console.log(store.getState())
+    const state = store.getState()
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+    console.log(visibleExpenses)
 });
 
 const expenseOne = store.dispatch(addExpense({
     description: 'Rent',
-    amount: 100
+    amount: 100,
+    createdAt: 1000
 }));
 
 const expenseTwo = store.dispatch(addExpense({
     description: 'Coffee',
-    amount: 300
+    amount: 300,
+    createdAt: -1000
 }));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }))
 
-// store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter('ffe'));
 // store.dispatch(setTextFilter());
 
 // store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 
-store.dispatch(setStartDate(125))
-store.dispatch(setStartDate())
-store.dispatch(setEndDate(125))
-store.dispatch(setEndDate())
+// store.dispatch(setStartDate(0))
+// store.dispatch(setStartDate())
+// store.dispatch(setEndDate(999))
+// store.dispatch(setEndDate())
 
 const demoState = {
     expenses: [{
@@ -183,14 +202,3 @@ const demoState = {
         endDate: undefined
     }
 };
-
-const user = {
-    name: "JEN",
-    age: 29
-}
-
-console.log({
-    ...user,
-    location: "Ulsan",
-    age: 39
-})
