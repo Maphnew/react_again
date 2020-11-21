5ee593c94282bc6d537b6d9180d899e436c4dcb6
maphnew <zcm3118@gmail.com>
2020-11-21 17:21:44 +0900

    Setup production webpack build

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path')
+const ExtractTestPlugin = require('extract-text-webpack-plugin')
 
 module.exports = (env)=> {
     const isProduction = env === 'production';
-    
+    const CSSExtract = new ExtractTestPlugin('styles.css');
     return {
         entry: './src/app.js',
         output: {
@@ -16,14 +17,28 @@ module.exports = (env)=> {
                 exclude: /node_modules/ 
             }, {
                 test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
             }]
         },
-        devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             historyApiFallback: true
