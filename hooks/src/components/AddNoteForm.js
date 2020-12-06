446ec67ff2268a65d5d8e57151d3d23ca7479a3a
maphnew <zcm3118@gmail.com>
2020-12-06 15:37:21 +0900

    Hooks useContext

---
+++ hooks/src/components/AddNoteForm.js
@@ -0,0 +1,31 @@
+import React, {useContext, useState} from 'react';
+import NoteContext from '../context/notes-context'
+
+const AddNoteForm = () => {
+    const { dispatch } = useContext(NoteContext)
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+
+    const addNote = (e) => {
+        e.preventDefault()
+        dispatch({
+            type: 'ADD_NOTE',
+            title,
+            body
+        })
+        setTitle('')
+        setBody('')
+    }
+    return (
+        <div>
+            <p>Add note</p>
+            <form onSubmit={addNote}>
+                <input value={title} onChange={(e) => setTitle(e.target.value)} />
+                <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+                <button>add note</button>
+            </form>
+        </div>
+    )
+}
+
+export { AddNoteForm as default }
